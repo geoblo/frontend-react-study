bb741bfaf90132284bacfc980ba1cf2108bf2a23
geoblo <geoblo@naver.com>
2023-05-24 17:03:13 +0900

    업로드

---
+++ 10_React/my-app/src/chapter5/5.3/Layout.jsx
@@ -0,0 +1,11 @@
+function Layout(props) {
+  return (
+    <>
+      {}
+      레이아웃의 크기는 가로 {}, 세로 {}
+      {}
+    </>
+  );
+}
+
+export default Layout;
--- 10_React/my-app/src/index.js
+++ 10_React/my-app/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import JsxUse from './chapter3/3.4/JsxUse';
+import Library from './chapter3/Library';
+import Clock from './chapter4/Clock';
+import PropsUse from './chapter5/5.3/PropsUse';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // Root DOM Node에 렌더링 하도록 하는 함수
@@ -13,8 +16,23 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // );
 
 // 3장 예제
+// root.render(
+//   // <JsxUse />
+//   <Library />
+// );
+
+// 4장 예제
+// 1초마다 Clock 컴포넌트를 렌더링 하는 코드
+// (참고로 실제 개발에서 이렇게 쓰는 경우는 없음)
+// setInterval(() => {
+//   root.render(
+//     <Clock />
+//   );
+// }, 1000);
+
+// 5장 예제
 root.render(
-  <JsxUse />
+  <PropsUse />
 );
 
 // If you want to start measuring performance in your app, pass a function
