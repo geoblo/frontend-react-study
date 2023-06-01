89297c77b70732c36f872625149b2395c2ccce2c
geoblo <geoblo@naver.com>
2023-06-01 19:00:50 +0900

    수업 업로드

--- 10_React/my-post/src/App.js
+++ 10_React/my-post/src/App.js
@@ -10,9 +10,10 @@ import PostDetail from './components/PostDetail';
 function App() {
   // 서버에서 가져온 데이터라고 가정
   const [posts, setPosts] = useState(['리액트 잘 쓰려면?', '자바스크립트 핵심 문법', '스타일링 가이드']);
-
   const [showPostDetail, setShowPostDetail] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(null);
+  const [likeCount, setLikeCount] = useState([0, 0, 0]);
+  const [inputValue, setInputValue] = useState('');
 
   return (
     <>
@@ -57,9 +58,70 @@ function App() {
               <h4>{post}</h4>
               <p>2023년 1월 20일</p>
               <p>by goni.kim</p>
+
+              <hr />
+
+              <div className="toolbar">
+                {/* 좋아요 기능 만들기 */}
+                <span onClick={(e) => {
+                  // (버그 수정) 현재는 좋아요 버튼 누를때 글 상세보기까지 같이 클릭됨!!
+                  // 부모-자식 관계에 있을 때 이벤트 버블링이 일어남
+                  e.stopPropagation(); // 상위 요소로 퍼지는 이벤트 버블링을 막음
+
+                  const copyLikeCount = [...likeCount]; // 배열의 복사본 만들기(새로운 배열)
+                  copyLikeCount[index]++;
+                  setLikeCount(copyLikeCount);
+                }}>💗 {likeCount[index]}</span>
+
+                {/* 포스트 삭제하기 */}
+                <span style={{ fontSize: 27 }}
+                  onClick={(e) => {
+                    // div를 직접 제거하는 것 X
+                    // state에서 제거하면 알아서 자동으로 렌더링 O
+                    e.stopPropagation();
+
+                    const copyPosts = [...posts];
+                    copyPosts.splice(index, 1);
+                    setPosts(copyPosts);
+                    // 또는 배열의 filter() 함수 사용
+                    // const filteredPosts = posts.filter((value, idx) => {
+                    //   return idx !== index;
+                    // });
+                    // setPosts(filteredPosts);
+
+                    // (버그 수정) 삭제 시 해당 포스트의 좋아요 카운트도 같이 삭제
+                    const copyLikeCount = [...likeCount];
+                    copyLikeCount.splice(index, 1);
+                    setLikeCount(copyLikeCount);
+                  }}
+                >🗑</span>
+              </div>
             </div>
           );
         })}
+
+        {/* 포스트 등록하기 */}
+        {/* Quiz: 제목 입력 후 등록 버튼 누르면 맨 앞에 새로운 포스트 추가 */}
+        {/* 
+          1. 제어 컴포넌트로 만들어서 사용자가 입력한 값을 state로 저장해서 관리 
+          2. 등록 버튼 클릭 시 posts 상태에 맨 앞에 새로운 데이터 추가
+        */}
+        <input type='text' value={inputValue} onChange={e => setInputValue(e.target.value)} />
+        <button onClick={() => {
+          // div 하나를 새로 생성 X
+          // posts state에 요소 하나 추가하면 자동으로 렌더링 O
+          // const copyPosts = [...posts];
+          // copyPosts.unshift(inputValue);
+          const copyPosts = [inputValue, ...posts];
+          setPosts(copyPosts);
+          setInputValue('');
+
+          // (버그 수정) 포스트 하나 추가 시 좋아요 카운트도 같이 추가
+          const copyLikeCount = [0, ...likeCount];
+          setLikeCount(copyLikeCount);
+        }}>
+          포스트 등록
+        </button>
 
         {/* 포스트 상세보기 */}
         {showPostDetail && <PostDetail posts={posts} currentIndex={currentIndex} setPosts={setPosts} />}
@@ -69,3 +131,21 @@ function App() {
 }
 
 export default App;
+
+// <추가 개선 과제>
+// 1. PostListItem 컴포넌트 추출
+
+// 2. 날짜 및 작성자, 좋아요 수 등 데이터를 문자열이 아닌 객체 형태로 처리해보기
+// state에 글 제목만 저장되어 있는게 아니라 날짜같은 것도 저장해두고 보여주는 식이면 굿
+// 글 등록 시 현재 날짜까지 같이 저장되도록 하면 나이스
+
+// 3. input에 아무것도 입력안하고 등록 버튼 누르는거 막기
+// 유저의 의도치않은 행동을 막는 코드도 많이 짜야 안전한 사이트가 됨
+// 1) 미입력시 비활성화 -> 입력이 생기면 버튼 활성화
+// 2) 등록 버튼 누를 시 유효성 검사
+
+// 4. 포스트 수정할 때 input으로 입력받은 내용으로 수정해보기
+
+// 5. 글 오름차순 정렬
+
+// 6. 그 외 개선 및 구현할 것 많으니 자유롭게 연습
