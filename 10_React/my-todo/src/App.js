17b7458d942cca544095113e40de5d6baffea5ec
geoblo <geoblo@naver.com>
2023-06-07 15:29:44 +0900

    업로드

--- 10_React/my-todo/src/App.js
+++ 10_React/my-todo/src/App.js
@@ -4,7 +4,7 @@ import reset from "styled-reset";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
 
 // 패키지 설치
 // npm install styled-components styled-reset react-icons
@@ -48,13 +48,49 @@ function App() {
     },
   ]);
 
+  // 새 객체를 만들 때마다 id값에 1씩 더해주어야 하는데 useRef()를 사용하여 변수 생성
+  // Quiz: state가 아닌 이유? id값은 렌더링되는 정보가 아니기 때문(화면에 보이지도 않고, 이 값이 바뀐다고 해서 컴포넌트가 재렌더링 될 필요도 없음)
+  // 단순히 새로운 항목을 만들 때 참조되는 값임
+  const nextId = useRef(4);
+  console.log(nextId);
+
+  // props로 전달해야 할 함수를 만들 때는 useCallback()을 사용해본다!
+  const handleInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false
+    };
+
+    // 방법1 - 이전까지 쓰던 방법
+    // const copyTodos = [...todos];
+    // copyTodos.push(todo);
+    // setTodos(copyTodos); // 새로운 배열을 만들어 넣어줌
+
+    // 방법2 - 배열의 내장 함수 이용
+    // 함수형 업데이트를 쓸 경우 의존성을 제거해 줄 수 있다!
+    setTodos(todos => todos.concat(todo)); // 새로운 배열을 반환함
+
+    nextId.current += 1; // nextId에 1씩 더하기
+  }, []);
+
+  const handleRemove = useCallback((id) => {
+    // 방법1
+    const copyTodos = [...todos];
+    const targetIndex = todos.findIndex((todo) => todo.id === id);
+    copyTodos.splice(targetIndex, 1);
+    setTodos(copyTodos);
+
+  }, [todos]);
+
+
   return (
     <>
       {/* <Reset /> */}
       <GlobalStyle />
       <TodoTemplate>
-        <TodoInsert />
-        <TodoList todos={todos} />
+        <TodoInsert onInsert={handleInsert} />
+        <TodoList todos={todos} onRemove={handleRemove} />
       </TodoTemplate>
     </>
   );
--- 10_React/my-todo/src/components/TodoInsert.jsx
+++ 10_React/my-todo/src/components/TodoInsert.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { MdAdd as AddIcon } from "react-icons/md";
 // Tip: as를 사용하여 별칭을 붙여 사용하면 추후 아이콘 바꿀때 한곳만 바꾸면 되서 편함!
@@ -42,10 +42,28 @@ const StyledButton = styled.button`
 
 // 새로운 항목을 입력하고 추가할 수 있는 컴포넌트
 // state를 통해 input의 상태를 관리
-function TodoInsert(props) {
+function TodoInsert({ onInsert }) {
+  const [value, setValue] = useState('');
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    onInsert(value);
+    setValue(''); // value값 초기화
+
+    // submit 이벤트가 발생시키는 새로고침을 방지
+    e.preventDefault();
+  };
+
   return (
-    <TodoInsertWrapper>
-      <StyledInput type='text' placeholder='할 일을 입력하세요.' />
+    <TodoInsertWrapper onSubmit={handleSubmit}>
+      <StyledInput type='text' 
+        placeholder='할 일을 입력하세요.' 
+        value={value} 
+        onChange={handleChange} 
+      />
       <StyledButton type='submit'>
         <AddIcon />
       </StyledButton>
