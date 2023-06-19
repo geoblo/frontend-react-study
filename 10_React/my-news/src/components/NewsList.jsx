73f4deac3a528b0c8204b952ce0f6eb2c2271c7e
geoblo <geoblo@naver.com>
2023-06-19 15:36:12 +0900

    css

---
+++ 10_React/my-news/src/components/NewsList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from "styled-components";
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+function NewsList(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default NewsList;
