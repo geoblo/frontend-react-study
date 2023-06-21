a3ecb139d864f35e98d01a461c2045c8a9fc68e1
geoblo <geoblo@naver.com>
2023-06-21 15:41:01 +0900

    업로드

--- 10_React/my-shop/src/pages/Main.js
+++ 10_React/my-shop/src/pages/Main.js
@@ -26,7 +26,9 @@ function Main(props) {
       <section>
         <Container>
           <Row>
-          <Col md={4}>
+            {/* 부트스트랩을 이용한 반응형 작업 */}
+            {/* md >= 768px 이상에서 전체 12등분중 4:4:4로 보여줌 */}
+            <Col md={4}>
               <img src="https://www.yonexmall.com/shop/data/goods/1645767865278s0.png" width="80%" />
               <h4>상품명</h4>
               <p>상품가격</p>
