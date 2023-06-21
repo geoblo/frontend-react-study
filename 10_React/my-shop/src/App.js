9f4219e479639b19759a4b152de2b451a65a27c0
geoblo <geoblo@naver.com>
2023-06-21 14:27:01 +0900

    업로드

---
+++ 10_React/my-shop/src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
+
+// ## Bootstrap
+// 레이아웃을 복사 붙여넣기 식으로 편하게 개발 가능한 css, js 라이브러리
+// 리액트 용이 따로 있는데 나는 기존것이 더 익숙하다 싶으면 기존 Bootstrap을 써도 무관
+// https://react-bootstrap.netlify.app/
+
+// ## 패키지 설치하기
+// npm install react-bootstrap bootstrap styled-components react-router-dom @reduxjs/toolkit react-redux axios
