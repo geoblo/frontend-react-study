9eddf3e788d55d77f48eabb5583cf554fc7d31d8
geoblo <geoblo@naver.com>
2023-07-12 15:57:19 +0900

    필기

--- 11_TypeScript/1_types.ts
+++ 11_TypeScript/1_types.ts
@@ -7,3 +7,47 @@
 
 // 타입스크립트에서 지원하는 기본 자료형
 // boolean, number, string, Array, object, 튜플, enum, any, void, null과 undefined, never 등이 있음
+
+// 1. boolean
+// true 또는 false 값을 저장하는 타입
+let active: boolean = true;
+// 이 변수에는 boolean 타입만 들어올 수 있음
+// active = 123; // 실수로 타입을 변경하면 에러 발생
+
+// 2. number
+// 숫자 데이터를 저장하는 타입
+// 10진, 16진, 2진, 8진수 리터럴을 지원
+let decimal: number = 10;
+let hex: number = 0xa;
+let binary: number = 0b1010;
+let octal: number = 0o12;
+// 이 변수에는 number 타입만 들어올 수 있음
+console.log(decimal, hex, binary, octal);
+
+// 3. string
+// 문자열 데이터를 저장하는 타입
+let message: string = 'Welcome';
+// 이 변수에는 string 타입만 들어올 수 있음
+
+// 4. Array
+// 배열의 타입을 정의하는 방법에는 2가지가 존재
+// 1) 첫번째 방법: type[]
+let firstArray: number[] = [1, 2, 3]; // 숫자만 담긴 배열만 가능
+
+// 2) 두번째 방법(제네릭): Array<type>
+let secondArray: Array<number> = [4, 5, 6]; // 숫자만 담긴 배열만 가능
+// 제네릭: 타입을 파라미터로 입력하는 문법
+
+// 5. object
+let person: object = { a: 11, b: 'bb', c: true };
+// 이 변수에는 원시 자료형이 아닌 모든 자료형 값이 들어올 수 있음(예: 객체, 배열, 함수 등)
+person = [1, 2, 3];
+person = () => {};
+
+// 원시 자료형이란?
+// person = 1;
+// person = 'aa';
+// person = true;
+// person = undefined;
+// person = null;
+// (참고) https://hwani.dev/js-primitive-reference-types/
